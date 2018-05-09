8c66e1e6e90e978d44a2391a4774299d0c1a4121
Travis Trumbly <t.trumbly@gmail.com>
2018-05-08 20:16:06 -0700

    Tuesday Night, pre MVP

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,26 +1,22 @@
 from flask import Flask, render_template
-from flask.ext.sqlalchemy import SQLAlchemy
+#from flask.ext.sqlalchemy import SQLAlchemy
 import numpy as np
 import pandas as pd
 import os
+from sklearn.ensemble import ExtraTreesClassifier
 
 #Model for Forest Identifying#
-#put stuff here about modeling a forest#
 train = pd.read_csv('train 3.csv')
 train['Id'] = train['Id'].apply(str)
 train['Cover_Type'] = train['Cover_Type'].apply(str)
-X_train = train.drop(['Cover_Type', 'Id', 'Dist_To_Water'],1)
+X_train = train.drop(['Cover_Type', 'Id'],1)
 y_train = train['Cover_Type']
 FOREST = ExtraTreesClassifier(n_estimators=200, random_state=42).fit(X_train, y_train)
-
-
+# End of Forest Model
 
 app = Flask(__name__)
 
 @app.route('/')
-def main():
-    return render_template('index.html')
-
 def viz_page():
     """
     Visualization page for the app
@@ -35,17 +31,9 @@ def score():
     '''
     data = flask.request.json
     x = np.matrix(data['example'])
-    score = FOREST.predict_proba(x)
-
-#app.config.from_object(os.environ['APP_SETTINGS'])
-#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-#db = SQLAlchemy(app)
-
-#from models import Result
-
-@app.route('/<name>')
-def hello_name(name):
-    return 'Hello {}!'.format(name)
+    score = FOREST.predict(x)
+    results = {'score':score[1,2,3,4,5,6,7]}
+    return flask.jsonify(results)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
